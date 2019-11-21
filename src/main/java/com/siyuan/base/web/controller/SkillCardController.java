c92ab7ee37bbc35b0de56991840807f645b992d1
思远 <twlinight@163.com>
2019-11-21 22:19:53 +0800

    1.

--- src/main/java/com/siyuan/base/web/controller/SkillCardController.java
+++ src/main/java/com/siyuan/base/web/controller/SkillCardController.java
@@ -1,14 +1,10 @@
 package com.siyuan.base.web.controller;
 
 import com.siyuan.base.biz.service.SkillCardService;
-import com.siyuan.base.dao.entity.SkillCard;
-import com.siyuan.base.dao.repository.SkillCardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/skill")
@@ -20,6 +16,6 @@ public class SkillCardController {
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String getAccounts() {
         System.out.println(skillCardService.saveSkillInfo());
-        return "新增成功";
+        return "static/index.html";
     }
 }
