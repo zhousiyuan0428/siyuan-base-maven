07d2005a4ac05d393767415b87d424cf5ff0f4c5
思远 <twlinight@163.com>
2019-11-30 19:33:29 +0800

    2. 测试类的加入

---
+++ src/test/java/SkillTest.java
@@ -0,0 +1,13 @@
+import com.siyuan.base.biz.service.SkillCardService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SkillTest extends ApplicationTests {
+    @Autowired
+    private SkillCardService skillCardService;
+    @Test
+    public void test11(){
+        skillCardService.saveSkillInfo();
+        //Assert.assertSame("企业数量有误",500,);
+    }
+}
