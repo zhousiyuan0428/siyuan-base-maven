691ae4301496484ff094df1b5fd4ba6875900a4f
思远 <twlinight@163.com>
2019-12-01 09:51:32 +0800

    2. 测试类的加入

--- src/test/java/ApplicationTests.java
+++ src/test/java/ApplicationTests.java
@@ -1,3 +1,4 @@
+import com.siyuan.base.Application;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
@@ -5,9 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-//由于是Web项目，Junit需要模拟ServletContext，因此我们需要给我们的测试类加上@WebAppConfiguration
-//@WebAppConfiguration
+@SpringBootTest(classes = Application.class)
 public class ApplicationTests {
     @Before
     public void init() {
